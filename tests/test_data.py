0cf5d3a81575ffe81229362ad2ce5eacb81ee259
Hsinyu Chen <chenxinyu.tw@gmail.com>
2024-04-15 17:11:58 +0200

    recover setting in test_data.py back to >= 5000

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,4 +5,4 @@ from tests.test_fixtures import testdata
 
 def test_get_data_length(testdata):
     model_store, X_train, X_test, y_train, y_test = testdata
-    assert len(X_train) <= 1000, f"training data X_train has {len(X_train)} rows, which is less than 5k rows"
+    assert len(X_train) >= 5000, f"training data X_train has {len(X_train)} rows, which is less than 5k rows"
